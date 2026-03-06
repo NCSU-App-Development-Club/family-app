32678c0bfa22348367b75f6bab753138eda9d592
Omar Abou <odabou@ncsu.edu>
2026-03-05 19:50:52 -0500

    Testing the calendar UI

--- mobile/app/calendar.tsx
+++ mobile/app/calendar.tsx
@@ -1,10 +1,25 @@
 import { ThemedText } from '@/components/themed-text'
 import { ThemedView } from '@/components/themed-view'
+// import { IoIosNotifications } from 'react-icons/io'
 
 export default function Calendar() {
   return (
     <ThemedView>
-      <ThemedText type="title">Calendar page contents</ThemedText>
+      {/* <ThemedText type="title">Calendar page contents </ThemedText> */}
+
+      <Header />
     </ThemedView>
+  )
+}
+
+function Header() {
+  const name: string = 'Testing'
+
+  return (
+    <>
+      <ThemedView>
+        <ThemedText>{name}</ThemedText>
+      </ThemedView>
+    </>
   )
 }
