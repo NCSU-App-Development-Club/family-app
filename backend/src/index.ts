5f820380256b315c768b9633ba048971eb85f0b4
Omar Abou <omarabou0503@gmail.com>
2026-04-02 20:02:08 -0400

    Added route for getting the current family group info

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,11 +3,13 @@ import { Hono } from "hono";
 
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/libsql";
+import { eq } from "drizzle-orm";
 import { auth } from "./auth.js";
 import { zValidator } from "@hono/zod-validator";
 import { z } from "zod";
-
-const db = drizzle(process.env.DB_FILE_NAME!);
+import { GetGroupRequestSchema, GetGroupResponse } from "@family-app/types";
+import { db } from "./db/database.js";
+import { group } from "./db/schema.js";
 
 const app = new Hono();
 
@@ -22,6 +24,20 @@ app.on(["POST", "GET"], "/api/auth/*", (c) => auth.handler(c.req.raw));
 // - Create a new calendar event
 // - Fetch the data for a calendar event
 // - Fetch info about your current family group
+app.get("/api/group/:groupId", async (c) => {
+  const groupId = Number(c.req.param("groupId"));
+
+  const groupData = await db.select().from(group).where(eq(group.id, groupId));
+
+  if (groupData.length === 0) {
+    return c.json({ error: "Group not found" }, 404);
+  }
+
+  return c.json({
+    name: groupData[0].name,
+    users: [],
+  } satisfies GetGroupResponse);
+});
 // - Invite another person to your family group (adds an entry to the groupInvitation table)
 // - Accept an invitation to a family group
 // - Leave your current family group
