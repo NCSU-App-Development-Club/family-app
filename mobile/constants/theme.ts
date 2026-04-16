f8437b1a1b84b307c95d8a6ad4e4286553415be5
ZachNurk <142443751+ZachNurk@users.noreply.github.com>
2026-04-16 15:40:27 -0400

    Login page (#1)
    * Added login UI
    
    * Update login flow UI
    
    * Small comment to make UI easier to understand
    
    * fixing up documentation
    
    * made suggested changes, not really sure how to make UI look better ATM though.
    
    * made suggested changes, didnt test on IOS because wifi is bad. Maybe need to make UI nicer
    
    * added password field
    
    * Fixed bug for login not loading
    
    * Fixed scrolling issues
    
    * npm
    
    ---------
    
    Co-authored-by: pdpate22 <pdpate22@ncsu.edu>
    Co-authored-by: parthpatel0123456 <pdpatel2006@hotmail.com>

--- mobile/constants/theme.ts
+++ mobile/constants/theme.ts
@@ -16,6 +16,8 @@ export const Colors = {
     icon: '#687076',
     tabIconDefault: '#687076',
     tabIconSelected: tintColorLight,
+    buttonBackground: '#55CFCB',
+    buttonText: '#FFFFFF',
   },
   dark: {
     text: '#ECEDEE',
@@ -24,6 +26,8 @@ export const Colors = {
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',
     tabIconSelected: tintColorDark,
+    buttonBackground: '#3A9E9A',
+    buttonText: '#FFFFFF',
   },
 }
 
